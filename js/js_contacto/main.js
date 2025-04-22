128d7ec63cd01527e34e011f75708aa237beb18b
Bruno Velasco <seccion891@gmail.com>
2025-04-22 16:28:54 -0500

    Se agrega un alert de bootstrap para mostrar un mensaje de error al ingresar informacion incorrecta

--- js/js_contacto/main.js
+++ js/js_contacto/main.js
@@ -67,7 +67,30 @@ function validarCorreo(){
     
 }//validarCorreo
 
+function mostrarError(mensajeError) {
+    let formulario = document.getElementsByClassName("row justify-content-center").item(0);
+    
+    formulario.insertAdjacentHTML("afterend", 
+        `
+        <div class="alert alert-danger" role="alert" id="error-msg">
+            ${mensajeError}
+        </div>
+        `
+    );
+}
+
+function ocultarError() {
+    try {
+        error_msg = document.getElementById("error-msg");
+        error_msg.remove();
+      } catch (error) {
+        
+      }
+}
+
 btnEnviar.addEventListener("click", function(event){
+    let mensajeError = "";
+    ocultarError();
 
     event.preventDefault();
 
@@ -82,24 +105,28 @@ btnEnviar.addEventListener("click", function(event){
     //Validmos que la longitud del valor del nombre sea mayor a 1
     if(txtName.value.length < 1){
         isValid = false;
+        mensajeError += "<p>El nombre es muy corto</p>";
     }//length<3
-
+    
     //Validmos que la longitud del valor del email sea mayor a 1
     if(! validarCorreo()){
         isValid = false;
+        mensajeError += "<p>El correo es invalido</p>";
     }//length<3
-
+    
     if(! validarNumero()){
         isValid = false;
+        mensajeError += "<p>El numero telefónico es invalido</p>";
     }//validarNumero
-
+    
     //Validmos que la longitud del valor del email sea mayor a 1
     if(txtMessage.value.length < 1){
         isValid = false;
+        mensajeError += "<p>El mensaje es muy corto</p>";
     }//length<3
 
     if(isValid){
-        
+
         let elemento = {
                         "Nombre" : txtName.value,
                         "Email" : txtEmail.value,
@@ -126,6 +153,8 @@ btnEnviar.addEventListener("click", function(event){
             icon: "success"
         });
         
+    } else {
+        mostrarError(mensajeError);
     }
 
 });//btnEnviar

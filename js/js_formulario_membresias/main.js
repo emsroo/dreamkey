8a63ce2c18c511fc43fe33d5bb23e669ff3c8abf
LUU <luuosoriio@gmail.com>
2025-04-30 22:42:37 -0600

    se crea html formulario-membresía y js_formulario_membresías

---
+++ js/js_formulario_membresias/main.js
@@ -0,0 +1,13 @@
+//Variable de acceso al elemento del nombre
+const txtName = document.getElementById("nom");
+//Variable de acceso al elemento  precioPublico
+const  precioPublico = document.getElementById("precio-publico");
+//Variable de acceso al elemento  precioAfiliados
+const  precioAfiliados = document.getElementById("precio-afiliados");
+//Variable de acceso al elemento descripcion
+const descripcion = document.getElementById("descripcion");
+
+//Variable de acceso al elemento imagen
+const btnEnviar = document.getElementById("btnEnviar");
+
+

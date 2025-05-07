7180f0f24c97fbf1e6da2137bde32600f328fc9e
LUU <luuosoriio@gmail.com>
2025-05-06 21:23:07 -0600

    Se agrega el modal en membresías

--- js/js_membresias/main.js
+++ js/js_membresias/main.js
@@ -5,7 +5,7 @@ export function addItem(item) {
                 <div class="card-header">
                     <h4 class="titulo">${item.name}</br> (${item.tipoMembresia})<span>
                         <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#staticBackdrop">Detalles</button>
+                             data-bs-target="#modal-${item.id}">Detalles</button>
                     </span></h4>
                 </div>
                 <div class="card-body">
@@ -19,26 +19,26 @@ export function addItem(item) {
         </div>
 
         
-        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <!-- Modal dinámico -->
+        <div class="modal fade" id="modal-${item.id}" tabindex="-1" aria-labelledby="modalLabel-${item.id}" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h1 class="modal-title fs-5" id="modalLabel-${item.id}">${item.name}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
                     </div>
-                    <div class="modal-body">
-                        ...
+                    <div class="modal-body text-center">
+                        <img src="${item.img}" class="img-fluid mb-3" alt="${item.name}" style="max-width: 200px;">
+                        <p>${item.description}</p>
+                       
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
+                        <button type="button" id="btnCerrar" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        
                     </div>
                 </div>
             </div>
         </div>
-
-
     `;
 
     const itemsContainer = document.getElementById("list-items");
@@ -46,56 +46,62 @@ export function addItem(item) {
 }
 
 addItem({
+    id: "1",
     name: 'PAQUETE 1',
     tipoMembresia: 'Básico',
     img: './assets/img-memebresia/Membresia1.png',
-    description: 'Incluye 5 servicios rosas (básicos) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la pagina de afiliados que empresas están disponibles con este nivel.',
+    description: 'Incluye 5 servicios rosas (básicos) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.',
     price1: '$700',
     price2: '$500',
 });
 
 addItem({
+    id: "2",
     name: 'PAQUETE 2',
     tipoMembresia: 'Básico',
     img: './assets/img-memebresia/Membresia2.png',
-    description: 'Incluye 10 servicios rosas (básicos) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la pagina de afiliados que empresas están disponibles con este nivel.',
+    description: 'Incluye 10 servicios rosas (básicos) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.',
     price1: '$1,200',
     price2: '$800',
 });
 
 addItem({
+    id: "3",
     name: 'PAQUETE 3',
-    tipoMembresia: 'Estandar',
+    tipoMembresia: 'Estándar',
     img: './assets/img-memebresia/Membresia3.png',
-    description: 'Incluye 5 servicios platino (Estandar) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la pagina de afiliados que empresas están disponibles con este nivel.',
+    description: 'Incluye 5 servicios platino (Estandar) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.',
     price1: '$1,200',
     price2: '$800',
 });
 
 addItem({
+    id: "4",
     name: 'PAQUETE 4',
-    tipoMembresia: 'Estandar',
+    tipoMembresia: 'Estándar',
     img: './assets/img-memebresia/Membresia4.png',
-    description: 'Incluye 10 servicios platino (Estandar) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la pagina de afiliados que empresas están disponibles con este nivel.',
+    description: 'Incluye 10 servicios platino (Estandar) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.',
     price1: '$1,800',
     price2: '$1,200',
 });
 
 addItem({
+    id: "5",
     name: 'PAQUETE 5',
     tipoMembresia: 'Premium',
     img: './assets/img-memebresia/Membresia5.png',
-    description: 'Incluye 5 servicios oro (Premium) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la pagina de afiliados que empresas están disponibles con este nivel.',
+    description: 'Incluye 5 servicios oro (Premium) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.',
     price1: '$1,800',
     price2: '$1,200',
 });
 
 
 addItem({
+    id: "6",
     name: 'PAQUETE 6',
     tipoMembresia: 'Premium',
     img: './assets/img-memebresia/Membresia6.png',
-    description: 'Incluye 10 servicios oro (Premium) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la pagina de afiliados que empresas están disponibles con este nivel.',
+    description: 'Incluye 10 servicios oro (Premium) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.',
     price1: '$3,500',
     price2: '$2,000',
 });
